6b1cd966647adbc7677c6c1369d380917ed43ac4
IDGS-901-17001740 <17001740@alumnos.utleon.edu.mx>
2024-04-29 17:01:00 -0600

    Change validation post

--- form.php
+++ form.php
@@ -1,26 +1,27 @@
 <?php
 #Developed by Ulises Reyes
 require 'validations.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender =  $_POST['gender'];
+    $birthdate = $_POST['birthdate'];
 
-$name = $_POST['name'];
-$age = $_POST['age'];
-$gender =  $_POST['gender'];
-$birthdate = $_POST['birthdate'];
+    #Validamos los campos del formulario
+    $errors = [];
+    $errors['name'] = validate_name($name);
+    $errors['age'] = validate_age($age);
+    $errors['birthdate'] = validate_birthdate($birthdate);
 
-#Validamos los campos del formulario
-$errors = [];
-$errors['name'] = validate_name($name);
-$errors['age'] = validate_age($age);
-$errors['birthdate'] = validate_birthdate($birthdate);
+    #En caso de que no hubiera errores las funciones regresan una cadena vacia,
+    #eliminamos esas entradas
+    $errors = array_filter($errors);
 
-#En caso de que no hubiera errores las funciones regresan una cadena vacia,
-#eliminamos esas entradas
-$errors = array_filter($errors);
-
-#Validamos si los errores estan vacios
-if (empty($errors)) {
-    echo json_encode(['message' => 'Datos recibidos con exito']);
-} else {
-    // Hay errores, devuelve el arreglo de errores en formato JSON
-    echo json_encode(['errors' => $errors]);
+    #Validamos si los errores estan vacios
+    if (empty($errors)) {
+        echo json_encode(['message' => 'Datos recibidos con exito']);
+    } else {
+        // Hay errores, devuelve el arreglo de errores en formato JSON
+        echo json_encode(['errors' => $errors]);
+    }
 }
